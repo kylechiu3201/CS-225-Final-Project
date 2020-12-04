a5f9019fd086f58e05841c2bee14b69f6fbd9973
Kyle Chiu <kchiu6@illinois.edu>
2020-12-03 22:29:45 -0600

    formatted airport.h

--- airport.h
+++ airport.h
@@ -3,27 +3,26 @@
 
 using std::string;
 
-class Airport{
-    public:
-    Airport();
-    Airport(string data);
-    int get_port_ID();
-    string get_name();
-    string get_city();
-    string get_country();
-    string get_IATA();
-    string get_ICAO();
-    double get_latitude();
-    double get_longitude();
+class Airport {
+ private:
+  int port_ID;
+  string name_;
+  string city_;
+  string country_;
+  string IATA_;
+  string ICAO_;
+  double latitude_;
+  double longitude_;
 
-    private:
-    int port_ID;
-    string name_;
-    string city_;
-    string country_;
-    string IATA_;
-    string ICAO_;
-    double latitude_;
-    double longitude_;
-
+ public:
+  Airport();
+  Airport(string data);
+  int get_port_ID();
+  string get_name();
+  string get_city();
+  string get_country();
+  string get_IATA();
+  string get_ICAO();
+  double get_latitude();
+  double get_longitude();
 };
