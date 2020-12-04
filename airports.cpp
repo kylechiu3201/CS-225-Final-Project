780bb43cab1bb3d07b6cf29b51de6961f4cafdf1
Brandon Wong <bjwong3@linux-a2.ews.illinois.edu>
2020-12-04 02:23:40 -0600

    fixed getline

--- airports.cpp
+++ airports.cpp
@@ -3,25 +3,48 @@
 #include <iostream>
 #include <stdlib.h>
 
-Airports::Airports(string filename) : g_(true){
+using std::string;
+
+Airports::Airports(string filename ) : g_(true){
     std::ifstream file(filename);
     if(file.fail()){
         std::cout << "Error opening file. Quitting..." << std::endl;
         exit(EXIT_FAILURE);
         return;
     }
-    std::string str;
+    std::string str, name, city, country, IATA, ICAO;
+    int port;
+    double latitude, longitude;
+
     while(std::getline(file,str)){
-        Airport port(str);
+        std::stringstream ss(str);  // takes the line of data and puts them into fields
+        string data;
+        getline(ss, data, ',');
+        port = std::stoi(str);
+        getline(ss, data, ',');
+        name = data;
+        getline(ss, data, ',');
+        city = data;
+        getline(ss, data, ',');
+        country = data;
+        getline(ss, data, ',');
+        IATA = data;
+        getline(ss, data, ',');
+        ICAO = data;
+        getline(ss, data, ',');
+        std::stringstream(data) >> latitude;
+        getline(ss, data, ',');
+        std::stringstream(data) >> longitude;
+        Airport airport_(port, name, city, country, IATA, ICAO, latitude, longitude);
         //insert vertex function
-        g_.insertVertex(port);
+        g_.insertVertex(airport_);
     }
 }
 void Airports::bfs(){
 
 }
 void Airports::getDistance(string airport1, string airport2){
-    
+
 }
 void Airports::getStronglyConnected(string airline){
 
