5b093f519b6d65e3ad2386eb629cc932e601bc09
Brandon Wong <bjwong3@linux-a2.ews.illinois.edu>
2020-12-03 22:05:54 -0600

    getters and constructors

--- airport.h
+++ airport.h
@@ -4,11 +4,25 @@
 using std::string;
 
 class Airport{
+    public:
+    Airport();
+    Airport(string data);
+    int get_port_ID();
+    string get_name();
+    string get_city();
+    string get_country();
+    string get_IATA();
+    string get_ICAO();
+    double get_latitude();
+    double get_longitude();
+
     private:
     int port_ID;
     string name_;
     string city_;
     string country_;
+    string IATA_;
+    string ICAO_;
     double latitude_;
     double longitude_;
 
