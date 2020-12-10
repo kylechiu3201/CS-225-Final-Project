440df1021bc69157a717ff62486b70a10e27c3c2
Kyle Chiu <kchiu6@illinois.edu>
2020-12-10 12:26:16 -0600

    fixed main.cpp to accommodate exporting of strongly connected data

--- main.cpp
+++ main.cpp
@@ -32,36 +32,46 @@ int main() {
     // if(command == "getDist")
     //   airports.shortest_path();
     if (command == "allNeeded") {
-      std::cout << std::endl;
-      auto airlines = airports.airlinesAdded();
-      for (auto i : airlines) {
-        std::cout << i.first.get_name() << ":   \n";
-        for (auto j : i.second) {
-          std::string name = j.get_IATA();
-          if (name == "" || name == "\"\"" || name == "\\N")
-            name = j.get_ICAO();
-          if (name == "" || name == "\"\"" || name == "\\N") continue;
-          std::cout << j << std::endl;
-        }
-        std::cout << std::endl << std::endl << std::endl;
-      }
+      /* std::cout << std::endl; */
+      /* auto airlines = airports.airlinesAdded(); */
+      /* for (auto i : airlines) { */
+      /*   std::cout << i.first.get_name() << ":   \n"; */
+      /*   for (auto j : i.second) { */
+      /*     std::string name = j.get_IATA(); */
+      /*     if (name == "" || name == "\"\"" || name == "\\N") */
+      /*       name = j.get_ICAO(); */
+      /*     if (name == "" || name == "\"\"" || name == "\\N") continue; */
+      /*     std::cout << j << std::endl; */
+      /*   } */
+      /*   std::cout << std::endl << std::endl << std::endl; */
+      /* } */
+      std::string filename = airports.airlinesAdded("allAirlines");
+      if(filename == "empty")
+        std::cout << "\nNo airports needed to be added for any airlines.\n\n";
+      else
+        std::cout << "\nData exported to \"" << filename << "\".\n\n";
     } else if (command == "airlineNeeded") {
       std::cout << "Which airline? Use IATA or ICAO\n";
       std::string airline;
       std::cin >> airline;
-      vector<Vertex> ans = airports.getStronglyConnected("\"" + airline + "\"");
-      if (ans.empty())
-        std::cout << "No airports needed to add.";
-      else {
-        std::cout << std::endl;
-        for (auto i : ans) {
-          std::string name = i.get_IATA();
-          if (name == "" || name == "\"\"" || name == "\\N")
-            name = i.get_ICAO();
-          if (name == "" || name == "\"\"" || name == "\\N") continue;
-          std::cout << i << std::endl;
-        }
-      }
+      /* vector<Vertex> ans = airports.getStronglyConnected("\"" + airline + "\""); */
+      /* if (ans.empty()) */
+      /*   std::cout << "No airports needed to add."; */
+      /* else { */
+      /*   std::cout << std::endl; */
+      /*   for (auto i : ans) { */
+      /*     std::string name = i.get_IATA(); */
+      /*     if (name == "" || name == "\"\"" || name == "\\N") */
+      /*       name = i.get_ICAO(); */
+      /*     if (name == "" || name == "\"\"" || name == "\\N") continue; */
+      /*     std::cout << i << std::endl; */
+      /*   } */
+      /* } */
+      std::string filename = airports.airlinesAdded("\"" + airline + "\"");
+      if(filename == "empty")
+        std::cout << "\nNo airports needed to be added for the selected airline.\n\n";
+      else
+        std::cout << "\nData exported to \"" << filename << "\".\n\n";
     } else if (command == "bfs")
       airports.bfs();
     else if (command == "exit")
