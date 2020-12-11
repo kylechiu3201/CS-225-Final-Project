b653197dcd7c03f4385d6175127640ae29a6593d
Kyle Chiu <kchiu6@illinois.edu>
2020-12-11 12:35:38 -0600

    commented airline files

--- airline/airline.cpp
+++ airline/airline.cpp
@@ -1,6 +1,9 @@
 #include "airline.h"
 
+// Creates an empty Airline object with all default values
 Airline::Airline() {}
+
+// Creates an Airline object with the given parameter values
 Airline::Airline(int ID, std::string name, std::string IATA, std::string ICAO,
                  std::string country) {
   ID_ = ID;
@@ -10,35 +13,40 @@ Airline::Airline(int ID, std::string name, std::string IATA, std::string ICAO,
   country_ = country;
 }
 
+// Returns the Airline ID
 int Airline::get_ID() { return ID_; }
 
+// Returns the Airline name
 std::string Airline::get_name() const { return name_; }
 
+// Returns the Airline IATA
 std::string Airline::get_IATA() const { return IATA_; }
 
+// Returns the Airline ICAO
 std::string Airline::get_ICAO() const { return ICAO_; }
 
+// Returns the Airline country
 std::string Airline::get_country() const { return country_; }
 
-/* bool Airline::operator>(const Airline& a) const { return ID_ > a.ID_; } */
-
+// Compares the IATA/ICAO of the Airline for operator>
 bool Airline::operator>(const Airline& a) const {
   if (IATA_ != "" && a.IATA_ != "") return IATA_ > a.IATA_;
   return ICAO_ > a.ICAO_;
 }
 
+// Compares the IATA/ICAO of the Airline for operator==
 bool Airline::operator==(const Airline& a) const {
   if (IATA_ != "" && a.IATA_ != "") return IATA_ == a.IATA_;
   return ICAO_ == a.ICAO_;
 }
 
-/* bool Airline::operator<(const Airline& a) const { return ID_ < a.ID_; } */
-
+// Compares the IATA/ICAO of the Airline for operator<
 bool Airline::operator<(const Airline& a) const {
   if (IATA_ != "" && a.IATA_ != "") return IATA_ < a.IATA_;
   return ICAO_ < a.ICAO_;
 }
 
+// Overloads << for easier output of the data
 ostream& operator<<(ostream& out, const Airline& obj) {
   out << obj.ID_ << "," << obj.name_ << ",\\N," << obj.IATA_ << "," << obj.ICAO_
       << ",\\N," << obj.country_ << ",\"Y\"";
--- airline/airline.h
+++ airline/airline.h
@@ -6,23 +6,35 @@ using std::ostream;
 
 class Airline {
  public:
+  // Creates an empty Airline object with all default values
   Airline();
+  // Creates an Airline object with the given parameter values
   Airline(int ID, std::string name, std::string IATA, std::string ICAO,
           std::string country);
+  // Returns the Airline ID
   int get_ID();
+  // Returns the Airline name
   std::string get_name() const;
+  // Returns the Airline IATA
   std::string get_IATA() const;
+  // Returns the Airline ICAO
   std::string get_ICAO() const;
+  // Returns the Airline country
   std::string get_country() const;
+  // Compares the IATA/ICAO of the Airline for operator>
   bool operator>(const Airline& a) const;
+  // Compares the IATA/ICAO of the Airline for operator<
   bool operator<(const Airline& a) const;
+  // Compares the IATA/ICAO of the Airline for operator==
   bool operator==(const Airline& a) const;
 
  private:
+  // Instance variables
   int ID_;
   std::string name_;
   std::string IATA_;
   std::string ICAO_;
   std::string country_;
+  // Overloads << for easier output of the data
   friend ostream& operator<<(ostream& out, const Airline& obj);
 };
