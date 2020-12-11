40f0236f8cdb1a7fbcb805c7122c6463a8782ccc
Kyle Chiu <kchiu6@illinois.edu>
2020-12-11 13:27:26 -0600

    formatted and commented

--- airport/airport.h
+++ airport/airport.h
@@ -6,97 +6,182 @@ using std::istream;
 using std::ostream;
 using std::string;
 
+/**
+ * @brief Airport class for airport data
+ */
 class Airport {
  public:
-  // Creates an empty Airport object with default values
+  /**
+   * @brief Constructor for default Airport object
+   */
   Airport();
 
-  // Creates an Airport object with the given parameters
+  /**
+   * @brief Constructor for Airport object with given parameters
+   *
+   * @param port The ID of the airport
+   * @param name The name of the airport
+   * @param city The city of the airport
+   * @param country The country of the airport
+   * @param IATA The IATA code of the airport
+   * @param ICAO The ICAO code of the airport
+   * @param latitude The latitude coordinate of the airport
+   * @param longitude The longitude coordinate of the airport
+   */
   Airport(int port, string name, string city, string country, string IATA,
           string ICAO, long double latitude, long double longitude);
 
-  // Gets the ID of the airport
+  /**
+   * @brief Gets the ID of the airport
+   *
+   * @return The ID of the airport
+   */
   int get_port_ID() const;
 
-  // Gets the name of the airport
+  /**
+   * @brief Gets the name of the airport
+   *
+   * @return The name of the airport
+   */
   string get_name() const;
 
-  // Gets the city of the airport
+  /**
+   * @brief Gets the city of the airport
+   *
+   * @return The city of the airport
+   */
   string get_city() const;
 
-  // Gets the country of the airport
+  /**
+   * @brief Gets the country of the airport
+   *
+   * @return The country of the airport
+   */
   string get_country() const;
 
-  // Gets the IATA code of the airport
+  /**
+   * @brief Gets the IATA code of the airport
+   *
+   * @return The IATA code of the airport
+   */
   string get_IATA() const;
 
-  // Gets the ICAO code of the airport
+  /**
+   * @brief Gets the ICAO code of the airport
+   *
+   * @return The ICAO code of the airport
+   */
   string get_ICAO() const;
 
-  // Gets the latitude coordinate of the airport
+  /**
+   * @brief Gets the latitude coordinate of the airport
+   *
+   * @return The latitude coordinate of the airport
+   */
   long double get_latitude() const;
 
-  // Gets the longitude coordinate of the airport
+  /**
+   * @brief Gets the longitude coordinate of the airport
+   *
+   * @return The longitude coordinate of the airport
+   */
   long double get_longitude() const;
 
-  // Gets the distance from one airport to another
+  /**
+   * @brief Gets the distance from one airport to another
+   *
+   * @param a The first airport
+   * @param b The second airport
+   * @param units The units of the distance
+   *
+   * @return The distance from one airport to another
+   */
   static long double get_distance(const Airport& a, const Airport& b,
                                   char units);
 
-  // Gets the label of the airport
+  /**
+   * @brief Gets the label of the airport
+   *
+   * @return The label of the airport
+   */
   string get_label() const;
 
-  // Sets the latitude coordinate of the airport
+  /**
+   * @brief Sets the latitude coordinate of the airport
+   *
+   * @param latitude The latitude coordinate of the airport
+   */
   void set_lat(long double latitude);
 
-  // Sets the longitude coordinate of the airport
+  /**
+   * @brief Sets the longitude coordinate of the airport
+   *
+   * @param longitude The longitude coordinate of the airport
+   */
   void set_long(long double longitude);
 
-  // Sets the label of the airport
+  /**
+   * @brief Sets the label of the airport
+   *
+   * @param l The label of the airport
+   */
   void set_label(string l);
 
-  // Compares if current airport < given airport
+  /**
+   * @brief Compares if the current airport < the chosen airport
+   *
+   * @param a The chosen airport to compare to
+   *
+   * @return If the current airport < the chosen airport
+   */
   bool operator<(const Airport& a) const;
 
-  // Compares if current airport > given airport
+  /**
+   * @brief Compares if the current airport > the chosen airport
+   *
+   * @param a The chosen airport to compare to
+   *
+   * @return If the current airport > the chosen airport
+   */
   bool operator>(const Airport& a) const;
 
-  // Compares if current airport == given airport
+  /**
+   * @brief Compares if the current airport == the chosen airport
+   *
+   * @param a The chosen airport to compare to
+   *
+   * @return If the current airport == the chosen airport
+   */
   bool operator==(const Airport& a) const;
 
-  // Compares if current airport != given airport
+  /**
+   * @brief Compares if the current airport != the chosen airport
+   *
+   * @param a The chosen airport to compare to
+   *
+   * @return If the current airport != the chosen airport
+   */
   bool operator!=(const Airport& a) const;
 
  private:
-  // The ID of the airport
   int port_ID;
-
-  // The name of the airport
   string name_;
-
-  // The city of the airport
   string city_;
-
-  // The country of the airport
   string country_;
-
-  // The IATA code of the airport
   string IATA_;
-
-  // The ICAO code of the airport
   string ICAO_;
-
-  // The latitude coordinate of the airport
   long double latitude_;
-
-  // The longitude coordinate of the airport
   long double longitude_;
-
-  // The label of the airport
   string label;
 
-  // Formats output of the Airport object data for easier outputting
+  /**
+   * @brief Formats output of Airport data for easier outputting
+   *
+   * @param out The stream to output to
+   * @param obj The Airport object to output
+   *
+   * @return The outputted stream
+   */
   friend ostream& operator<<(ostream& out, const Airport& obj);
-
-  //friend istream& operator>>(istream& in, const Airport& obj);
+  // friend istream& operator>>(istream& in, const Airport& obj);
 };
